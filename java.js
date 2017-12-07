366bb172588462f18c09cb43ab6875fcd2ec4b2c
rc16je <31972315+rc16je@users.noreply.github.com>
2017-12-06 22:37:08 -0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+	<meta http-equiv="Content-type" content="text/html;charset=UTF-8" />
+	<title> Date and Time</title>
+	<link href="./basic_files/styles.css" rel="stylesheet" type="text/css" />
+    <script type="text/javascript" src="scripts.js"></script>
+</head>
+
+<body>
+
+<p id="demo"></p>
+	<script>
+var d = new Date("October 13, 2014 11:13:00");
+document.getElementById("demo").innerHTML = d;
+</script>
+</body>
+
+</html>
